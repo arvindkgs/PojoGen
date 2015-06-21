d05523ea2316aa9187bdcc11421a505bd30e475f
arvind <arvind@localhost>
2015-06-22 00:49:51 +0530

    Json Reader Parser

--- src/org/rigel/util/jsongen/parser/JsonReader.java
+++ src/org/rigel/util/jsongen/parser/JsonReader.java
@@ -1,5 +1,18 @@
 package org.rigel.util.jsongen.parser;
 
 public class JsonReader {
-
+	public void read(String json){
+		StringBuffer buf = new StringBuffer();
+		int valStrtI;
+		int valEndI;
+		int valueNameI;
+		int keyStrtI;
+		int keyEndI;
+		int lastFbIndex;
+		
+		int p = 0;
+		do{
+			
+		}while(p<json.length());
+	}
 }
